094ee3b78bff48d6d676a20c53172ab4359f28fc
unknown <stacy.perales@ynov.com>
2018-03-06 11:45:07 +0100

    coucou

---
+++ TP2/exo1.php
@@ -0,0 +1,18 @@
+
+<h3>Calculatrice</h3>
+
+<form action="exo1php.php" method="POST">
+ <p>Valeur 1 : <input type="text" name="v1" /></p>
+ <p>Valeur 2 : <input type="text" name="v2" /></p>
+ <p>Résultat : _____________________</p>
+ <div>
+ <p><input type="submit" value="Additionner"></p>
+ <p><input type="submit" value="Multiplier"></p>
+</div>
+</form>
+
+<?php
+
+
+
+ ?>
