c6945509d2b42fcc840bafabb8e8e69bc40ac508
unknown <stacy.perales@ynov.com>
2018-03-08 11:42:58 +0100

    hello

--- TP2/exo1.php
+++ TP2/exo1.php
@@ -1,18 +1,19 @@
 
 <h3>Calculatrice</h3>
 
-<form action="exo1php.php" method="POST">
+<form  method="POST">
  <p>Valeur 1 : <input type="text" name="v1" /></p>
  <p>Valeur 2 : <input type="text" name="v2" /></p>
- <p>Résultat : _____________________</p>
+ <p>Résultat : <?php $_POST['sub'] ?></p>
  <div>
- <p><input type="submit" value="Additionner"></p>
- <p><input type="submit" value="Multiplier"></p>
+ <p><input type="submit" value="Additionner" name="sub"></p>
+ <p><input type="submit" value="Multiplier" name="sub"></p>
 </div>
 </form>
 
 <?php
-
-
+$valeur1 = $_POST['v1'];
+$valeur2 = $_POST['v2'];
+$_POST['sub']
 
  ?>
