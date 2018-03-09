1236a0d7690d7a4562b1e65583d4f7124f036270
unknown <stacy.perales@ynov.com>
2018-03-09 11:27:01 +0100

    salutsalut

--- TP1/exo6/Exo6.php
+++ TP1/exo6/Exo6.php
@@ -1,15 +1,38 @@
+<form  action='<?php echo $_SERVER["PHP_SELF"]; ?>' method='post'>
+
+    <p> Etes-vous heureux?<br>
+      <input type='radio' name='sexe' value='oui'>Oui<br>
+      <input type='radio' name='sexe' value='non'>Non<br>
+      <input type='radio' name='sexe' value='maybe'>Peut-être<br>
+    </p><br>
+
+    <p> Etes-vous triste?<br>
+      <input type='radio' name='sexe' value='oui'>Oui<br>
+      <input type='radio' name='sexe' value='non'>Non<br>
+      <input type='radio' name='sexe' value='maybe'>Peut-être<br>
+    </p><br>
+
+    <p> Existe-t-il des gens heureux?<br>
+      <input type='radio' name='sexe' value='oui'>Oui<br>
+      <input type='radio' name='sexe' value='non'>Non<br>
+    </p><br>
+
+    <button type='submit' name='button'>Répondre</button>
+  </form>
+
 <?php
+echo "<!DOCTYPE html><html><head><meta charset='utf-8'><title>Formulaire</title><link rel='stylesheet'></head><body>";
 $s1 = array("question"=>"Êtes-vous heureux ?", "reponse"=>array("oui", "non", "peut-être"));
 $s2 = array("question"=>"Êtes-vous triste ?", "reponse"=>array("oui", "non"));
 $s3 = array("question"=>"Existe-t-il des gens heureux ?", "reponse"=>array("oui", "non"));
 $sondages = array($s1, $s2, $s3);
 $t = rechercheSondage($sondages, "heureux");
 rechercheSondage($sondages,"heureux");
+foreach($t as $sondage) genererFormulaire($sondage);
 
-foreach ($t as $sondage) genererFormulaire($sondage);
+//test des fonctions
 
-    function genererFormulaire($sondage)
-    {
+    function genererFormulaire($sondage){
         echo "<form>".$sondage['question'];
 
         for ($i = 0; $i <sizeof($sondage["reponse"]); $i++) {
@@ -24,4 +47,5 @@ foreach ($t as $sondage) genererFormulaire($sondage);
 }
 
 function rechercheSondage(){}
+
 ?>
--- TP2/exo1.php
+++ TP2/exo1.php
@@ -1,10 +1,9 @@
 
 <h3>Calculatrice</h3>
-
-<form  method="POST">
+<form  action='<?php echo $_SERVER["PHP_SELF"]; ?>' method='post'>
  <p>Valeur 1 : <input type="text" name="v1" /></p>
  <p>Valeur 2 : <input type="text" name="v2" /></p>
- <p>Résultat : <?php $_POST['sub'] ?></p>
+ <p>Résultat : <?php  ?></p>
  <div>
  <p><input type="submit" value="Additionner" name="sub"></p>
  <p><input type="submit" value="Multiplier" name="sub"></p>
@@ -12,8 +11,20 @@
 </form>
 
 <?php
-$valeur1 = $_POST['v1'];
-$valeur2 = $_POST['v2'];
-$_POST['sub']
+// Il faut utiliser if et isset.
+
+$valeur1 = $_POST["v1"];
+$valeur2 = $_POST["v2"];
+$addition = $valeur1 + $valeur2;
+$multiplication = $valeur1 * $valeur2;
+
+if (isset($_POST["sub"])) {
+    $resultat = $addition;
+    echo "$resultat";
+}
+if (isset($_POST["sub"])) {
+    $resultat = $multiplication;
+    echo "$resultat";
+}
 
  ?>
