3ac40efe64ffa1352e80fca244c4dadd9cf8057d
Oznerologos <lorenzo.logos@ynov.com>
2018-03-14 18:15:14 +0100

    tp3 exo4

---
+++ TP3/exo1/exo1.php
@@ -0,0 +1,41 @@
+<?php
+  $nom = '';
+  $fichier = './file.txt';
+  $ressource = fopen('compteur.txt','r+');
+  $NombreClics = fgets($ressource);
+  $ListeNom = fopen('log.txt', 'a');
+
+  if (!empty($_POST['download']) && isset($_POST['download'])) {
+    if (!empty($_POST['nom']) && isset($_POST['nom'])) {
+      $nom = $_POST['nom'];
+      $NombreClics++;
+      fseek($ressource,0);
+      fputs($ressource,$NombreClics);
+      fclose($ressource);
+      fwrite($ListeNom, $nom.PHP_EOL);
+      fclose($ListeNom);
+      if (file_exists($fichier)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($fichier).'"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($fichier));
+        readfile($fichier);
+        exit;
+      }
+    }
+    else {echo "Veuillez saisir un nom";}
+  }
+
+?>
+
+<form  method="post">
+    <label for="nom">Nom :</label>
+    <input type="text" name="nom" id="nom"  placeholder="Nom">
+    <input type="submit" value="Télécharger" name="download">
+</form>
+
+
+<?php echo "Ce fichier a ete consulte : ".$NombreClics; ?>
---
+++ TP3/exo4/exo4.php
@@ -0,0 +1,67 @@
+
+
+<?php
+$couleur = "black";
+$taille = "30px";
+if (isset($_POST['color']) AND isset($_POST['size'])) {
+  $couleur = $_POST['color'];
+  setcookie("couleur", $couleur);
+  $taille = $_POST['size'];
+  setcookie("taille", $taille);
+} else if (isset($_COOKIE['couleur']) AND isset($_COOKIE['taille'])) {
+  $couleur = $_COOKIE['couleur'];
+  $taille = $_COOKIE['taille'];
+};
+
+function createCSS(){
+  if (!file_exists("exo4_css.php")) {
+    $open = fopen("exo4_css.php", "w");
+    fwrite($open,
+    '<?php
+
+    header("Content-type: text/css; charset=iso-8859-1");
+
+    if (isset($_COOKIE["couleur"])) {
+      $couleur = $_COOKIE["couleur"];
+    };
+    if (isset($_COOKIE["taille"])) {
+      $taille = $_COOKIE["taille"];
+    };
+
+    echo "body{color:".$couleur.";font-size:".$taille.";}";?>'
+    );
+  }
+};
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 4</title>
+  </head>
+  <body>
+    <span> Bonjour, vous pouvez ici choisir vos preferences, selectionnez une couleur et une taille de police</span> <br><br>
+    <form method="post">
+      <select name="color">
+        <option value="red">Rouge</option>
+        <option value="blue">Bleu</option>
+        <option value="black">Noir</option>
+      </select>
+
+      <select name="size">
+        <option value="15px">Petite</option>
+        <option value="30px">Moyene</option>
+        <option value="60px">Grande</option>
+      </select><br><br>
+      <input type="submit" name="change" value="Changer">
+    </form><br>
+    <a href="test.php">Testez ici !!</a>
+
+<?php createCSS() ?>
+
+  </body>
+</html>
